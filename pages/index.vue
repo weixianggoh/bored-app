3d5a07cd2d0769ddbbf1f8c23fb179168e8c367d
Goh Wei Xiang <weixianggoh1995@gmail.com>
2022-08-16 13:20:00 +0800

    add history store

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,9 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+
+  }
+}
 </script>
---
+++ store/history.js
@@ -0,0 +1,27 @@
+export const ADD_TO_HISTORY = "ADD_TO_HISTORY";
+export const REMOVE_FROM_HISTORY = "REMOVE_FROM_HISTORY";
+
+export const state = () => ({
+  data: []
+});
+
+export const getters = {
+  history: (state) => {
+    return state.data;
+  }
+};
+
+export const actions = {
+  addToHistory: function ({ commit }, payload) {
+    commit(ADD_TO_HISTORY, payload);
+  },
+  removeFromHistory: function ({ commit }, payload) {
+    commit(REMOVE_FROM_HISTORY, payload);
+  }
+};
+
+export const mutations = {
+  [ADD_TO_HISTORY](state, payload) {
+    state.data.push(payload);
+  }
+};
