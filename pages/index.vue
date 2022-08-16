6699e21d08ddb91a85ab4ad25ba709b6030f1fb4
Goh Wei Xiang <weixianggoh1995@gmail.com>
2022-08-16 13:45:18 +0800

    show list of activity

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    <a-button type="primary" @click="getRandom">
+    <a-page-header
+      style="border: 1px solid rgb(235, 237, 240)"
+      title="Bored? We are here to help"
+    />
+
+    <a-button type="primary" @click="getRandom" block>
       Next
     </a-button>
-    <pre>
-      {{ history }}
-    </pre>
+
+    <a-list item-layout="horizontal" :data-source="history">
+      <a-list-item slot="renderItem" slot-scope="item, index" style="padding: 10px">
+        <a-list-item-meta>
+          <a slot="title">{{ item.activity }}</a>
+          <div slot="description">RM{{ item.price }}</div>
+
+<!--          <a-avatar-->
+<!--            slot="avatar"-->
+<!--            src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"-->
+<!--          />-->
+        </a-list-item-meta>
+      </a-list-item>
+    </a-list>
+
+
   </div>
 </template>
 
@@ -15,6 +33,7 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   computed: {
     ...mapGetters("history", ["history"]),
+
   },
   methods: {
     ...mapActions("history", ["addToHistory"]),
