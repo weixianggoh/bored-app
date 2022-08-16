485ce11f20ff291087670b32fb4d7e58f8443a81
Goh Wei Xiang <weixianggoh1995@gmail.com>
2022-08-16 13:26:31 +0800

    call bored api

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
-  <Tutorial/>
+  <a-button type="primary" @click="getRandom">
+    Next
+  </a-button>
 </template>
 
 <script>
 export default {
   methods: {
-
+    getRandom() {
+      this.$message.loading("getting bored? stay tune...", 5000);
+      this.$axios
+        .$get(`http://www.boredapi.com/api/activity/`)
+        .then((result) => {
+          console.log("result");
+          console.log(result);
+          this.$message.destroy();
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   }
 }
 </script>
